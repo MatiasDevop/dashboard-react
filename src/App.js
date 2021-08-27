a9056f647401e5ea811e4719e6a607c7edb6ed87
Nestor Matias <solomatias321@gmail.com>
2021-08-27 12:41:13 +0200

    adding routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
